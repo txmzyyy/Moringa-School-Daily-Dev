11f85624e065a480f33439cbe8c2745b2eaf1f7d
jamescoder92 <kusimbajames92@gmail.com>
2026-08-20 11:37:36 +0300

    install dependancies

---
+++ src/routes/ProtectedRoute.jsx
@@ -0,0 +1,16 @@
+import { Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+export default function ProtectedRoute({ children, allowedRoles }) {
+  const { isAuthenticated, user } = useSelector((state) => state.auth);
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  if (allowedRoles && !allowedRoles.includes(user?.role)) {
+    return <Navigate to="/home" replace />;
+  }
+
+  return children;
+}
